389dc02b1e59d2975812d0e17da092e0afe09c95
SanteriSil <silkkueune@gmail.com>
2025-10-29 20:49:15 +0200

    Update Employee.java to prevent circular references between entities and add controller for Employee

---
+++ backend/proseed/src/main/java/com/proseed/controllers/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.proseed.controllers;
+
+import com.proseed.entities.Employee;
+import com.proseed.repos.EmployeeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
