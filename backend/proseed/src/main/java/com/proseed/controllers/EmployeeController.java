9d6cfa85c018fef36732140714fef8e9f2f51d4a
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-01 14:59:56 +0200

    feat: enable CORS support in employee, process, and task controllers

--- backend/proseed/src/main/java/com/proseed/controllers/EmployeeController.java
+++ backend/proseed/src/main/java/com/proseed/controllers/EmployeeController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
--- backend/proseed/src/main/java/com/proseed/controllers/TaskController.java
+++ backend/proseed/src/main/java/com/proseed/controllers/TaskController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
