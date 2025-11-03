9031e7041ca30ed93a016d4dd72569b8cb44d179
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-03 18:35:35 +0200

    feat: implement task editing functionality in TaskPage component

--- frontend/JSFrontend/src/components/TaskPage.jsx
+++ frontend/JSFrontend/src/components/TaskPage.jsx
@@ -1,5 +1,7 @@
-import {useParams} from "react-router";
-import {Link} from "react-router";
+import {Link, useParams} from "react-router";
+import {use, useState} from 'react';
+import {TaskContext} from "../Context/TaskContext/TaskContext.jsx";
+import EditTaskDetailsDialog from "./EditTaskDetailsDialog.jsx";
 
 /**
  * @component TaskPage
@@ -8,18 +10,55 @@ import {Link} from "react-router";
  * @returns {JSX.Element} The rendered TaskPage component.
  */
 export default function TaskPage() {
-    const {processId, taskId} = useParams();
+  const {processId, taskId} = useParams();
+  const {tasks, updateTask} = use(TaskContext);
+  const parsedTaskId = taskId ? parseInt(taskId) : undefined;
+  const foundTask = tasks.find(t => t.taskId === parsedTaskId);
+  const [isTaskDetailsDialogOpen, setIsTaskDetailsDialogOpen] = useState(false);
 
+  async function handleUpdateTask(newName, newDescription) {
+    if (newName === foundTask.taskName && newDescription === foundTask.taskDescription) {
+      setIsTaskDetailsDialogOpen(false);
+      return;
+    }
+    await updateTask(parsedTaskId, {
+      taskName: newName || foundTask.taskName,
+      taskDescription: newDescription || foundTask.taskDescription
+    });
+    setIsTaskDetailsDialogOpen(false);
+  }
+
+  if (!foundTask) {
     return (
-        <div>
-            <h1>task {taskId}</h1>
-            <p>This is the task page.</p>
-            <p>This is a description</p>
-            <Link to={`/process/${processId}`}>
-                <button>
-                    Go back to process {processId}
-                </button>
-            </Link>
-        </div>
+      <div>
+        <p>Task not found</p>
+        <Link to={`/process/${processId}`}>
+          <button>
+            Go back to process {processId}
+          </button>
+        </Link>
+      </div>
     )
+  }
+
+  return (
+    <div>
+      <Link to={`/process/${processId}`}>
+        <button>
+          Go back to process {processId}
+        </button>
+      </Link>
+      <h1>{foundTask.taskName}</h1>
+      <p>taskId: {taskId}</p>
+      <p>description: {foundTask.taskDescription}</p>
+      <button onClick={() => setIsTaskDetailsDialogOpen(true)}>Edit Task Details</button>
+      <EditTaskDetailsDialog
+        currentTaskName={foundTask.taskName}
+        currentTaskDescription={foundTask.taskDescription}
+        onSave={handleUpdateTask}
+        isOpen={isTaskDetailsDialogOpen}
+        onClose={() => setIsTaskDetailsDialogOpen(false)}
+      />
+    </div>
+  )
 }
