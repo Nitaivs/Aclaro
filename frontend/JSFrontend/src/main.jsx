5c34e80902c9351a86a660439a35077fdb904a88
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-09 13:38:49 +0200

    feat: wrap App component with EmployeeProvider for employee context

--- frontend/JSFrontend/src/main.jsx
+++ frontend/JSFrontend/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import {ProcessProvider} from "./Context/ProcessContext/ProcessProvider.jsx";
 import {TaskProvider} from "./Context/TaskContext/TaskProvider.jsx"
+import {EmployeeProvider} from "./Context/EmployeeContext/EmployeeProvider.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ProcessProvider>
       <TaskProvider>
-        <App/>
+        <EmployeeProvider>
+          <App/>
+        </EmployeeProvider>
       </TaskProvider>
     </ProcessProvider>
   </StrictMode>,
