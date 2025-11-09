6e86b314f3fd2ff8f1d7066248eae8e24477d82a
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-09 16:15:12 +0200

    feat: display a list of employees using context

--- frontend/JSFrontend/src/components/EmployeeListPage.jsx
+++ frontend/JSFrontend/src/components/EmployeeListPage.jsx
@@ -1,6 +1,24 @@
+import {use, useState} from "react";
+import {EmployeeContext} from "../Context/EmployeeContext/EmployeeContext.jsx";
+import Card from "@mui/material/Card";
 
 export default function EmployeeListPage() {
+  const {employees} = use(EmployeeContext);
+
   return (
-    <div>EmployeeListPage</div>
+    <div>
+      <h1>Employee list</h1>
+      <ul>
+        {employees.map(employee => (
+          <li key={employee.id}>
+            <Card
+              style={{margin: '10px', padding: '10px'}}
+            >
+            {employee.name}
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
