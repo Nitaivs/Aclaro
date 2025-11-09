264e1fd76c449ca218a3d788cc5cc87e427fdab3
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-09 15:13:14 +0200

    feat: add routes for EmployeeListPage and ProcessListPage in Router

--- frontend/JSFrontend/src/components/Router.jsx
+++ frontend/JSFrontend/src/components/Router.jsx
@@ -2,6 +2,8 @@ import {Route, Routes, BrowserRouter} from "react-router";
 import Dashboard from "./Dashboard.jsx";
 import ProcessPage from "./ProcessPage.jsx";
 import TaskPage from "./TaskPage.jsx";
+import EmployeeListPage from "./EmployeeListPage.jsx";
+import ProcessListPage from "./ProcessListPage.jsx";
 
 /**
  * @component Router
@@ -10,13 +12,15 @@ import TaskPage from "./TaskPage.jsx";
  * @returns {JSX.Element} The Router component.
  */
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/process/:processId" element={<ProcessPage/>}/>
-                <Route path="/process/:processId/task/:taskId" element={<TaskPage/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/processes" element={<ProcessListPage/>}/>
+        <Route path="/process/:processId" element={<ProcessPage/>}/>
+        <Route path="/process/:processId/task/:taskId" element={<TaskPage/>}/>
+        <Route path="/employees" element={<EmployeeListPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
