081372317ef6b2dae9e833754b351aee7b322170
Henaniemine <henkkamoi02@gmail.com>
2025-11-10 21:47:33 +0200

    Add: base roots for department pages. No functionality yet

---
+++ frontend/JSFrontend/src/components/DeparmentListPage.jsx
@@ -0,0 +1,4 @@
+import { use, useState } from "react";
+import { EmployeeContext } from "../Context/EmployeeContext/EmployeeContext.jsx";
+import AddEmployeeDialog from "./AddEmployeeDialog.jsx";
+import { Link } from "react-router";
