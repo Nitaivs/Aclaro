ace1a49db44ab6515f7a81b4664b8ab581472c43
Henaniemine <henkkamoi02@gmail.com>
2025-11-10 21:47:33 +0200

    Add: base roots for department pages. No functionality yet

---
+++ frontend/JSFrontend/src/components/DepartmentPage.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router";
+import { use, useEffect, useState } from 'react';
+import { DepartmentContext } from "../Context/DepartmentContext/DepartmentContext.jsx";
+import { useParams } from "react-router";
+//import EditDepartmentDialog from "./EditDepartmentDialog.jsx";
+//TODO: When ready above component is ready, uncomment
+
+
+export default function DepartmentPage() {
+    const { departments, updateDepartment } = useContext(DepartmentContext);
+    const { departmentId } = useParams();
+    const parsedDepartmentId = departmentId ? parseInt(departmentId) : undefined;
+    const foundDepartment = departments.find(d => d.id === parseInt(departmentId));
+    const [isEditDepartmentDialogOpen, setIsEditDepartmentDialogOpen] = useState(false);
+
+    function handleUpdateDepartment(updatedName) {
+        updateDepartment(parsedDepartmentId, updatedName);
+        setIsEditDepartmentDialogOpen(false);
+    }
+
+    return (
+        <div>
+            <h1>Department Page</h1>
+            <h2>Not ready yet :/</h2>
+        </div>
+    );
+}
