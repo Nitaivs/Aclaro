0704dd9155f88e383e26fcf009416ad9cba0298f
Henri Nieminen <henkkamoi02@gmail.com>
2025-11-10 13:08:04 +0200

    Add: employee list component to show all employees

---
+++ frontend/JSFrontend/src/components/Employeelist.jsx
@@ -0,0 +1,10 @@
+export default function EmployeeList(props) {
+
+    return (
+        <div>
+            <h1>Employee List Component</h1>
+            <h2>Nothing yet here, still being built</h2>
+        </div>
+    );
+
+}
