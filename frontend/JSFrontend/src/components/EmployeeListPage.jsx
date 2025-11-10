d3475a13904d32d9cd1b7dd51a87b2ab43419b8b
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-10 16:22:51 +0200

    feat: add navigation link to return to dashboard in EmployeeListPage

--- frontend/JSFrontend/src/components/EmployeeListPage.jsx
+++ frontend/JSFrontend/src/components/EmployeeListPage.jsx
@@ -1,12 +1,19 @@
 import {use, useState} from "react";
 import {EmployeeContext} from "../Context/EmployeeContext/EmployeeContext.jsx";
 import Card from "@mui/material/Card";
+import {Link} from "react-router";
 
 export default function EmployeeListPage() {
   const {employees} = use(EmployeeContext);
 
   return (
     <div>
+      <Link to={"/"}>
+        <button>
+          Return to dashboard
+        </button>
+      </Link>
+
       <h1>Employee list</h1>
       <ul>
         {employees.map(employee => (
