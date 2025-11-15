5121528b92a6ec649246eec807c7fd56f491f368
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-15 20:50:13 +0200

    feat: refactor ProcessNode to destructure data and pass parentTaskId to PlusButton

--- frontend/JSFrontend/src/components/ProcessNode.jsx
+++ frontend/JSFrontend/src/components/ProcessNode.jsx
@@ -1,7 +1,7 @@
 import {Handle, Position} from "@xyflow/react";
 import PlusButton from "./PlusButton.jsx";
 
-export default function ProcessNode({data}) {
+export default function ProcessNode({data: {label, processId }}) {
   return (
     <div style={{
       borderRadius: 6,
@@ -12,10 +12,10 @@ export default function ProcessNode({data}) {
       boxShadow: '2px 2px 5px rgba(0,0,0,0.3)'
     }}>
       <div style={{padding: 12, fontWeight: 'bold', textAlign: 'center', color: 'black'}}>
-        {data.label}
+        {label}
       </div>
       <Handle type="source" position={Position.Right}/>
-      <PlusButton onClick={() => console.log("clicked")}/>
+      <PlusButton parentTaskId={null} onClick={() => console.log("clicked")}/>
     </div>
   )
 }
