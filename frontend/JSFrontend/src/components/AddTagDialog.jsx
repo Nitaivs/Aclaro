240ad8286b026c6b84e29ecfa3fce32066c19c1c
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-20 16:35:39 +0200

    refactor: rename AddDepartmentDialog.jsx to AddTagDialog.jsx

---
+++ frontend/JSFrontend/src/components/AddTagDialog.jsx
@@ -0,0 +1,60 @@
+import {Dialog, DialogTitle, TextField} from '@mui/material';
+import {useState} from 'react';
+
+/**
+ * @Component AddDepartmentDialog
+ * @description A dialog component for adding a new department.
+ * @param onSave Callback function to handle saving the new department.
+ * @param isOpen Boolean to control the dialog open state.
+ * @param onClose Callback function to handle closing the dialog.
+ * @returns {JSX.Element} The AddDepartmentDialog component.
+ */
+
+export default function AddTagDialog({onSave, isOpen, onClose}) {
+  const [nameInput, setNameInput] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [selectedType, setSelectedType] = useState("department");
+
+  function handleOnSave() {
+    if (!nameInput) {
+      setNameError(true);
+      return;
+    }
+    onSave(nameInput);
+    handleClose();
+  }
+
+  function handleClose() {
+    setNameInput("");
+    setNameError(false);
+    onClose();
+  }
+
+  //Can currently only add names. No links to employees or skills, etc.
+  return (
+    <Dialog open={isOpen}>
+      <DialogTitle>Add a new tag</DialogTitle>
+      <div style={{padding: '0 24px 24px 24px'}}>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Department Name"
+          type="text"
+          fullWidth
+          variant="outlined"
+          required={true}
+          error={nameError}
+          helperText={nameError ? "Tag name is required" : ""}
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
+        <button onClick={() => handleOnSave()}>Add</button>
+        <button onClick={() => {
+          handleClose();
+        }}>
+          Cancel
+        </button>
+      </div>
+    </Dialog>
+  );
+}
