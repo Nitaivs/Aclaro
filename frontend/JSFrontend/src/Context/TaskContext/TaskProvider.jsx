c7ae54b3b900eecb710d7526c7840abf309b6986
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-27 20:38:36 +0200

    feat: refactor TaskProvider to utilize DataContext for improved task management

--- frontend/JSFrontend/src/Context/TaskContext/TaskProvider.jsx
+++ frontend/JSFrontend/src/Context/TaskContext/TaskProvider.jsx
@@ -1,162 +1,88 @@
-import {use, useState, useEffect} from "react";
+import {use} from "react";
 import {TaskContext} from "./TaskContext.jsx";
-import {ProcessContext} from "../ProcessContext/ProcessContext.jsx";
+import {DataContext} from "../DataContext/DataContext.jsx";
 import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/api/";
 
 /**
  * @Component TaskProvider
- * @description Provides task-related state and functions to its children via TaskContext.
- * @param children The child components that will have access to the task context.
+ * @description Provides state and functions related to tasks to its children via TaskContext.
+ * @param {Object} props The component props.
+ * @param {JSX.Element} props.children The child components that will have access to the task context.
  * @returns {JSX.Element} The TaskProvider component.
  */
 export function TaskProvider({children}) {
-  const BASE_URL = "http://localhost:8080/api/";
-  const [tasks, setTasks] = useState([]);
-  const [initialized, setInitialized] = useState(false);
-  const {fetchProcessById} = use(ProcessContext);
-
-
-  //TODO: improve error handling across all functions
-
-  /**
-   * Effect hook that initializes tasks from the database when the component mounts.
-   */
-  useEffect(() => {
-    if (!initialized) {
-      console.log("initializing tasks");
-      initializeTasksFromDB();
-    } else {
-      console.log("Tasks already initialized");
-    }
-  });
-
-  /**
-   * @function initializeTasksFromDB
-   * @description Initializes tasks from the database by fetching all tasks and setting the local state.
-   * Sets the initialized state to true once done.
-   * @returns {Promise<void>} A promise that resolves when the tasks are fetched and set
-   */
-  async function initializeTasksFromDB() {
-    try {
-      console.log("Initializing tasks from DB");
-      await fetchAllTasks();
-      setInitialized(true);
-    } catch (error) {
-      console.error("Error fetching tasks from DB:", error);
-    }
-  }
-
-  /**
-   * @function fetchAllTasks
-   * @description Fetches all tasks from the database and sets the local state.
-   * @returns {Promise<void>} A promise that resolves when the tasks are fetched and set
-   */
-  async function fetchAllTasks() {
-    try {
-      console.log("Fetching all tasks from DB");
-      const response = await axios.get(`${BASE_URL}tasks`);
-      console.log(`Tasks:`, response.data)
-      setTasks(response.data);
-    } catch (error) {
-      console.error("Error fetching tasks from DB:", error);
-    }
-  }
+  const {tasks, setTasks, fetchAllTasks, addTaskBetweenTasks} = use(DataContext);
 
   /**
    * @function addTask
-   * @description Adds a new task to the database and updates the local state.
-   * @param processId The ID of the process to which the task belongs. Required.
-   * @param name The name of the task. Required.
-   * @param description The description of the task. Default is null.
-   * @param parentTaskId The ID of the parent task, if any. Default is null.
-   * @returns {Promise<void>} A promise that resolves when the task is added and the state is updated.
+   * @description Adds a new task to a process. Makes a POST request to the backend with the task details,
+   * then fetches all tasks to update the state.
+   * @param {Number} processId - The ID of the process to which the task will be added. Expected to be an integer. Required.
+   * @param {string} name - The name of the new task. Required.
+   * @param {string} description - The description of the new task. Optional.
+   * @param parentTaskId - The ID of the parent task, if this task is a subtask. Optional.
+   * @returns {Promise<void>} A promise that resolves when the task is added and tasks are fetched.
    */
   async function addTask(processId, name, description = null, parentTaskId = null) {
-    if (!processId || !name) {
-      console.error("Process ID and task name are required to add a task.");
-      //TODO: throw error to inform user
-      throw new Error("Process ID and task name are required to add a task.");
-    }
     try {
-      console.log("Adding task to DB with processId:", processId, "name:", name, "description:", description, "parentTaskId:", parentTaskId);
-      const response = await axios.post(`${BASE_URL}tasks?processId=${processId}`, {
-        processId: processId,
+      if (!processId) {
+        throw new Error("Process ID is required to add a task.");
+      }
+
+      if (!name) {
+        throw new Error("Task name is required.");
+      }
+
+      if (parentTaskId !== null && isNaN(parentTaskId)) {
+        throw new Error("Provided parentTaskId is not a valid number.");
+      }
+
+      await axios.post(`${BASE_URL}tasks?processId=${processId}`, {
+        processId,
         name,
         description,
-        parentTaskId: parentTaskId
+        parentTaskId
       });
-      console.log("added task", response);
-      setTasks([...tasks, response.data]);
-      //TODO: hack to refresh tasks in process, rewrite
-      await fetchAllTasks()
-    } catch (error) {
-      console.error("Error adding task to DB:", error);
-    }
-  }
-
-  async function addTaskBetweenTasks(processId, name, description = null, parentTaskId, childTaskId) {
-    try {
-      if (!processId || !name) {
-        //TODO: inform user via toast notification and return instead of throwing new error (?)
-        throw new Error("Process ID and task name are required to add a task.");
-      }
-      if (!parentTaskId || !childTaskId) {
-        throw new Error("Parent task ID and child task ID are required to add a task between tasks.");
-      }
-      console.log("Adding task between tasks in DB with processId:", processId, "name:", name, "description:", description, "parentTaskId:", parentTaskId, "childTaskId:", childTaskId);
-      const response = await axios.post(`${BASE_URL}tasks/insert-between?parentTaskId=${parentTaskId}&childTaskId=${childTaskId}`, {
-        name,
-        description,
-      })
-
-      console.log("added task", response.data);
-
-      // await axios.put(`${BASE_URL}processes/${processId}`)
-
-      console.log("tasks before fetch:", tasks.length);
-      setTasks(prevTasks => [...prevTasks, response.data]);
       await fetchAllTasks();
-      console.log("tasks after fetch:", tasks.length);
     } catch (error) {
-      console.error("Error adding task between tasks:", error);
-      //TODO: add toast notification to inform user
+      console.error("Error adding task:", error);
+      throw error;
     }
   }
 
   /**
    * @function updateTask
-   * @description Updates an existing task in the database and updates the local state.
-   * @param taskId The ID of the task to update.
-   * @param updatedFields An object containing the fields to update.
-   * @returns {Promise<void>} A promise that resolves when the task is updated and the state is updated.
+   * @description Updates an existing task with new fields. Makes a PUT request to the backend,
+   * then updates the task in the state.
+   * @param {Number} taskId - The ID of the task to be updated. Expected to be an integer. Required.
+   * @param {Object} updatedFields - An object containing the fields to be updated. Required.
+   * @returns {Promise<void>} A promise that resolves when the task is updated.
    */
   async function updateTask(taskId, updatedFields) {
     try {
-      console.log("Updating task with ID:", taskId, "with fields:", updatedFields);
       const response = await axios.put(`${BASE_URL}tasks/${taskId}`, updatedFields);
-      console.log("Updated task:", response.data);
       setTasks(tasks.map(t => t.id === taskId ? response.data : t));
     } catch (error) {
-      console.error("Error updating task in DB:", error);
+      console.error("Error updating task:", error);
+      throw error;
     }
   }
 
   /**
    * @function deleteTask
-   * @description Deletes a task from the database and updates the local state.
-   * @param taskId The ID of the task to delete.
-   * @returns {Promise<void>} A promise that resolves when the task is deleted and the state is updated.
+   * @description Deletes a task by its ID. Makes a DELETE request to the backend,
+   * and then fetches all tasks to update the state.
+   * @param {Number} taskId - The ID of the task to be deleted. Expected to be an integer. Required.
+   * @returns {Promise<void>} A promise that resolves when the task is deleted and tasks are fetched.
    */
   async function deleteTask(taskId) {
     try {
-      console.log("Deleting task with ID:", taskId);
       await axios.delete(`${BASE_URL}tasks/${taskId}`);
-      setTasks(tasks.filter(t => t.taskId !== taskId));
-      //TODO: hack to refresh tasks in process, rewrite
       await fetchAllTasks();
     } catch (error) {
-      console.error("Error deleting task from DB:", error);
-      // Re-throw the error to inform the caller
+      console.error("Error deleting task:", error);
       throw error;
     }
   }
@@ -165,9 +91,9 @@ export function TaskProvider({children}) {
     <TaskContext.Provider value={{
       tasks,
       addTask,
-      addTaskBetweenTasks,
-      deleteTask,
       updateTask,
+      deleteTask,
+      addTaskBetweenTasks,
       fetchAllTasks
     }}>
       {children}
