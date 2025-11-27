7d48b4a96d45b02377b7891a52645646e221098f
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-27 19:57:55 +0200

    docs: add documentation to ProcessOperationsContext.jsx

--- frontend/JSFrontend/src/Context/ProcessOperationsContext/ProcessOperationsContext.jsx
+++ frontend/JSFrontend/src/Context/ProcessOperationsContext/ProcessOperationsContext.jsx
@@ -1,7 +1,17 @@
 import {createContext} from "react";
 
-export const ProcessOperationsContext = createContext();
+export const ProcessOperationsContext = createContext(undefined);
 
+/**
+ * @Component ProcessOperationsProvider
+ * @description Provides the process operations context to its children components.
+ * Provides the processId for operations related to a specific process.
+ * @param props
+ * @param children
+ * @param processId
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function ProcessOperationsProvider({ children, processId}) {
   return (
     <ProcessOperationsContext.Provider value={{processId}}>
