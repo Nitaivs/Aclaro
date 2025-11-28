0ed552930d825bab15e3e323c5c6fdf8bca78a34
Nitai Spira <nitaiv.spira@gmail.com>
2025-11-28 16:33:13 +0200

    style: rename TaskCard component to TaskItem and update structure for clarity

--- frontend/JSFrontend/src/components/TaskItem.jsx
+++ frontend/JSFrontend/src/components/TaskItem.jsx
@@ -1,17 +1,18 @@
 import Card from '@mui/material/Card';
 import {useNavigate, useLocation} from 'react-router';
+import '../style/ProcessItem.css'
 
 /**
  * @component TaskCard
  * @description A card component that displays information about a task within a process.
  * The card serves as a link to the detailed task page.
  * @param props The properties for the TaskCard component.
- * @param props.processId The ID of the process to which the task belongs.
- * @param props.taskId The ID of the task.
- * @param props.taskName The name of the task.
+ * @param {number} props.taskId The ID of the task.
+ * @param {string} props.taskName The name of the task.
+ * @param {string} props.taskDescription The description of the task.
  * @returns {JSX.Element} The rendered TaskCard component.
  */
-export default function TaskCard({ taskId, taskName, taskDescription}) {
+export default function TaskItem({taskId, taskName, taskDescription}) {
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -21,14 +22,16 @@ export default function TaskCard({ taskId, taskName, taskDescription}) {
 
   return (
     <div>
-      <Card
-        onClick={openTaskModal}
-        role="button"
-        style={{cursor: 'pointer'}}
-      >
-          <h2>{taskName}</h2>
+      <div className={"process-item"}>
+        <div className={"process-item-info"}>
+          <h4 style={{color: 'red'}}>Task</h4>
+          <span>|</span>
+          <h4>{taskName}</h4>
+        </div>
+        {/*<div className={"process-item-info"}>*/}
           <p>{taskDescription}</p>
-      </Card>
+        {/*</div>*/}
+      </div>
     </div>
   );
 }
